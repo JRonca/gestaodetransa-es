c7084104f015260486932272a93482f9846ad5e1
JRonca <jrfr21112000@gmail.com>
2020-04-24 04:05:41 -0300

    finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,39 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            accumulator.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
 export default TransactionsRepository;
+/* const balance = transactions.reduce(
+      (accumulator: Balance, currentValue: Transaction) => {
+        if (currentValue.type === 'income') {
+          accumulator.income += currentValue.value;
+        } else {
+          accumulator.outcome += currentValue.value;
+        }
+        accumulator.total = accumulator.income - accumulator.outcome;
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balance; */
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
-
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
+interface TransactionInterface {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: TransactionInterface): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    if (
+      type === 'outcome' &&
+      (await transactionsRepository.getBalance()).total < value
+    ) {
+      throw new AppError('insuficienteValue!');
+    } else {
+      const categoryRepository = getRepository(Category);
+      const findByCategory = await categoryRepository.findOne({
+        where: { title: category },
+      });
+      let category_id;
+      if (findByCategory) {
+        category_id = findByCategory.id;
+      } else {
+        const categoryCreate = categoryRepository.create({
+          title: category,
+        });
+        await categoryRepository.save(categoryCreate);
+        category_id = categoryCreate.id;
+      }
+      const transation = transactionsRepository.create({
+        title,
+        value,
+        type,
+        category_id,
+      });
+      await transactionsRepository.save(transation);
+      return transation;
+    }
   }
 }
 
